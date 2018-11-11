842a1d668d9f1cd37b7a47ea003dace87b7e0b68
fpawel <binf@mail.ru>
2018-11-11 20:11:45 +0300

    save

---
+++ elchese/EccCO.v2/Repository.fs
@@ -0,0 +1,132 @@
+﻿namespace EccCO.v2 
+
+module Repository =
+
+    open System
+    open System.IO
+    open DataModel
+
+    type Id = string
+
+    [<AutoOpen>]
+    module private Helpers = 
+        let usefile<'a> (path:string) (x:FileMode) (f : FileStream -> 'a)= 
+            try
+                use file = new FileStream( path, x ) 
+                f file |> Ok
+            with e ->             
+                printfn "Ошибка обращения к файлу %s, %A, %A" path x e 
+                Err e.Message
+
+        type FileName = { FileName : string }
+
+        let readFile<'T> filename (func : BinaryReader -> 'T)  =        
+            usefile filename.FileName FileMode.Open <| fun file ->
+                use reader = new BinaryReader(file)
+                func reader 
+
+        let writeFile<'T> filename (func : BinaryWriter -> unit)  =         
+            usefile filename.FileName FileMode.Create <| fun file ->        
+                use writer = new BinaryWriter(file)
+                func writer 
+
+        module Path = 
+            let data rootPath = 
+                let x = Path.Combine( rootPath, "Batches" ) 
+                if Directory.Exists x |> not then 
+                    Directory.CreateDirectory x |> ignore
+                x
+        
+            let batchFolder rootPath canCreate id (dt:DateTime)  = 
+                let (~%%) = string
+                let month = dt.ToString("MMM", System.Globalization.CultureInfo.InvariantCulture)
+                let path = Path.Combine(data rootPath, %% dt.Year, sprintf "%d-%s" dt.Month month, %% dt.Day, id )
+                if canCreate then
+                    createDirectory path
+                { FileName = path }
+
+            let batchFileName rootPath canCreate id dt  =             
+                { FileName = Path.Combine( (batchFolder rootPath canCreate id dt).FileName, sprintf "%s.batch"  id ) }
+            
+        let getBatchesInfo( rootPath ) =
+            let files = Directory.GetFiles( Path.data rootPath, "*.batch", SearchOption.AllDirectories)
+            let r = 
+                [   for filename in files do
+                        match   readFile {FileName=filename} FSharpBin.deserialize<BatchInfo>  with
+                        | Err x ->  ()                  
+                        | Ok x -> yield x ]   
+            r
+
+
+    module Path =
+        let year rootPath year = 
+            let x = Path.batchFolder rootPath false "-" (DateTime(year ,1,1) )
+            x.FileName 
+            |> Path.GetDirectoryName
+            |> Path.GetDirectoryName
+            |> Path.GetDirectoryName
+        let month rootPath year month = 
+            let x = Path.batchFolder rootPath false "-" (DateTime(year,month,1) )
+            x.FileName 
+            |> Path.GetDirectoryName
+            |> Path.GetDirectoryName
+
+        let day rootPath year month day = 
+            let x = Path.batchFolder rootPath false "-" (DateTime(year,month,day) )
+            x.FileName 
+            |> Path.GetDirectoryName
+
+        let batch rootPath id dateTime = 
+            (Path.batchFolder rootPath false id dateTime).FileName
+  
+    [<AutoOpen>]
+    module private Helpers1 = 
+        let load rootPath id dt =
+            let fileName = Path.batchFileName rootPath false id dt
+            if File.Exists fileName.FileName |> not then
+                Err ("не найден файл " + fileName.FileName)
+            else
+                readFile fileName <| fun reader ->
+                    let x = FSharpBin.deserialize<Batch> reader
+                    {   x with 
+                            Products = 
+                                [   for p in x.Products -> 
+                                        if p.Flash.Length = EEPROM_SIZE then p else
+                                        { p with Flash = Array.create EEPROM_SIZE 0xffuy}] }
+                
+                
+
+        let tryGetBatchInfo rootPath id = 
+            match getBatchesInfo rootPath |> List.tryFind( fun x -> x.Id=id) with
+            | None -> None
+            | Some batchInfo -> Some batchInfo
+
+    let getInfoList rootPath = 
+        List.rev ( getBatchesInfo rootPath)
+
+    let loadParty rootPath id date = load rootPath id date
+
+    let get rootPath id =
+        match tryGetBatchInfo rootPath id with
+        | None -> Err "партия не найдена"
+        | Some batchInfo ->
+            load rootPath id batchInfo.Date 
+
+    let save rootPath batch = 
+        let batchInfo = BatchInfo.creteNew batch
+        let fileName = Path.batchFileName rootPath true batchInfo.Id batchInfo.Date
+        try
+            let fileStream = new FileStream( fileName.FileName, FileMode.Create ) 
+            let writer = new BinaryWriter(fileStream)
+            FSharpBin.serialize writer batch
+            writer.Close()
+            fileStream.Close()
+
+        with e ->             
+            failwithf "write file %A: %s" fileName.FileName e.Message 
+            
+
+
+
+
+   
---
+++ elchese/db.fs
@@ -0,0 +1,336 @@
+﻿module db
+
+
+
+open System
+open System.IO
+open System.Data.SQLite
+open System.Data
+open System.Diagnostics
+open System.Data.SqlClient
+
+//let fileName = @"E:\Program Data\Аналитприбор\elco\elco.sqlite"
+//let repository_path = @"E:\User\Projects\VS2018\EccCO.v2\App\bin\Release" 
+// @"C:\Users\fpawel\Documents\Visual Studio 2015\Projects\Analit\EccCO.v2\App\bin\Release" 
+
+module old = 
+    open EccCO.v2
+    type PartyInfo = DataModel.BatchInfo
+    type Product = DataModel.Product
+    type Party = DataModel.Batch
+
+type private env = Environment
+
+type Context = 
+    {   conn : SQLiteConnection
+        importedOldParties:Set<string>
+        oldParties : old.PartyInfo list
+    }
+
+type private dbt = DbType
+
+type SQLiteCommand with
+    member cmd.AddValue name valueType (value:obj) = 
+        cmd.Parameters.Add(name, valueType).Value <- value
+
+    member cmd.AddParam (t:dbt) prm (v:'T option) =
+        match v with
+        | Some a -> 
+            cmd.Parameters.Add(prm, t).Value <- a
+        | _ -> 
+            cmd.Parameters.Add(prm, DbType.Object).Value <- null
+
+    member cmd.AddDecimalParam =
+        cmd.AddParam DbType.Decimal
+        
+
+let importProduct x (party_id:int64) (place:int) (old_product:old.Product) (old_party:old.Party) =         
+    match old_product.Serial with
+    | None -> ()
+    | Some serial ->
+        
+    let cmd = new SQLiteCommand(x.conn)
+    cmd.CommandText <- """
+            INSERT INTO product
+                (party_id, serial, place, product_type_name,  
+                    i13, i24, i35, i26, i17, not_measured, 
+
+                    i_f_minus20, i_f_plus20, i_f_plus50, i_s_minus20, i_s_plus20, i_s_plus50,
+
+                    flash, production, old_product_id, old_serial)
+            VALUES        
+                (@party_id, @serial, @place, @product_type_name,  
+                @i13, @i24, @i35, @i26, @i17, @not_measured, 
+                @i_f_minus20, @i_f_plus20, @i_f_plus50, @i_s_minus20, @i_s_plus20, @i_s_plus50,
+                @flash, @production, @old_product_id, @old_serial);
+                SELECT last_insert_rowid();"""
+
+    cmd.Parameters.Add("@party_id", dbt.Int64).Value <- party_id
+    if old_party.Products |> List.exists ( fun x -> x.N < old_product.N && x.Serial = Some serial) then
+        cmd.Parameters.Add("@serial", dbt.Object).Value <- null        
+    else
+        cmd.Parameters.Add("@serial", dbt.Int32).Value <- serial    
+    cmd.Parameters.Add("@place", dbt.Int64).Value <- place        
+        
+    cmd.AddParam dbt.String "@product_type_name" 
+        (   if old_product.ProductType = "" then 
+                None 
+            else Some old_product.ProductType
+        )
+
+    [   "@i13", old_product.I13
+        "@i24", old_product.I24
+        "@i35", old_product.I35
+        "@i26", old_product.I26
+        "@i17", old_product.I17
+        "@not_measured", old_product.In
+
+        "@i_f_minus20", old_product.If_20
+        "@i_f_plus20", old_product.Ifon
+        "@i_f_plus50", old_product.If50
+        "@i_s_minus20", old_product.Is_20
+        "@i_s_plus20", old_product.Isns
+        "@i_s_plus50", old_product.Is50 ] |> List.iter(fun (k,v) -> cmd.AddDecimalParam k v)
+
+    if old_product.Flash |> Array.exists( (<>) 0xffuy) then 
+        cmd.Parameters.Add("@flash", dbt.Binary, old_product.Flash.Length).Value <- old_product.Flash
+    else 
+        cmd.Parameters.AddWithValue("@flash", null) |> ignore   
+    cmd.Parameters.Add("@production", dbt.Boolean).Value <- old_product.IsReportIncluded
+    cmd.Parameters.Add("@old_product_id", dbt.String).Value <- old_product.Id
+    cmd.Parameters.Add("@old_serial", dbt.Int32).Value <- serial
+        
+    let new_product_id = cmd.ExecuteScalar() :?> int64
+
+    [   for a in old_product.CustomTermo do
+            yield a.T, (a.I, a.K)
+    ]
+    |> Map.ofList
+    |> Map.toList
+    |> List.iter (fun ( t,(i,k)) -> 
+        let cmd = new SQLiteCommand(x.conn)
+        cmd.CommandText <- """
+INSERT INTO product_temperature_current_k_sens (product_id, temperature, current, k_sens)
+VALUES (@product_id, @temperature, @current, @k_sens);"""
+        cmd.Parameters.Add("@product_id", dbt.Int64).Value <- new_product_id
+        cmd.Parameters.Add("@temperature", dbt.Decimal).Value <- t
+        cmd.AddDecimalParam "@current" i
+        cmd.AddDecimalParam "@k_sens" k
+        cmd.ExecuteNonQuery() |> ignore )
+
+let importParty x old_party_id old_party_date = 
+
+        let old_party = 
+            match EccCO.v2.Repository.loadParty 
+                env.CurrentDirectory old_party_id old_party_date with
+            | Err err -> failwith err 
+            | Ok x -> x
+        
+        let cmd = new SQLiteCommand(x.conn)
+    
+        cmd.CommandText <- """
+            
+            INSERT INTO party (created_at, old_party_id, product_type_name, concentration1, concentration2, concentration3, note) 
+                VALUES
+                    (@created_at, @old_party_id, @product_type_name, @concentration1, @concentration2, @concentration3, @note);
+            SELECT last_insert_rowid();"""
+
+        cmd.Parameters.Add("@created_at", DbType.DateTime).Value <- old_party.Date
+        cmd.Parameters.Add("@old_party_id", DbType.String).Value <- old_party.Id
+        cmd.Parameters.Add("@product_type_name", DbType.String).Value <- old_party.ProductType        
+        cmd.Parameters.Add("@concentration1", DbType.Decimal).Value <- old_party.PGS1
+        cmd.Parameters.Add("@concentration2", DbType.Decimal).Value <- old_party.PGS2
+        cmd.Parameters.Add("@concentration3", DbType.Decimal).Value <- old_party.PGS3
+        if old_party.Name <> "" then 
+            cmd.Parameters.Add("@note", DbType.String).Value <- old_party.Name
+        else
+            cmd.Parameters.Add("@note", DbType.Object).Value <- null      
+
+        let party_id =cmd.ExecuteScalar() :?> int64
+    
+        for place = 0 to old_party.Products.Length - 1 do
+            let old_product = old_party.Products.[place]
+            importProduct x party_id place  old_product  old_party 
+
+let importParties x = 
+
+    let cmd = new SQLiteCommand(x.conn, CommandText = "BEGIN TRANSACTION;")
+    cmd.ExecuteNonQuery() |> ignore
+
+    let startTime = DateTime.Now
+    
+    let percent  = 
+        let count = x.oldParties.Length
+        fun i ->                
+            ((float i / float count) * 100.) |> round |> int
+
+    let resetCursor =             
+        let conx, cony = Console.CursorLeft, Console.CursorTop
+        fun () ->
+            let conx2 = Console.CursorLeft
+            Console.SetCursorPosition(conx, cony)
+            printf "%s" (String(' ', conx2 - conx + 1))
+            Console.SetCursorPosition(conx, cony)            
+
+    x.oldParties |> List.iteri (fun i old_party ->            
+        let s = sprintf "%d %d%s %A  " (i+1) ( percent i) "%" old_party.Date
+        if Set.contains old_party.Id x.importedOldParties then
+            resetCursor()
+            printf "%s: skip" s
+        else
+            importParty x old_party.Id  old_party.Date
+            resetCursor()
+            printf "%s" s 
+        )  
+    
+    cmd.CommandText <- "COMMIT;"
+    cmd.ExecuteNonQuery() |> ignore
+    printfn ""
+    printfn "total time: %A" (DateTime.Now - startTime)
+
+let readExportProduct (r:SQLiteDataReader) = 
+    let (~%%) (s:string) : 'T option = 
+        if r.[s] = null then None else Some (r.[s] :?> 'T)        
+    let p = old.Product.createNew (r.["place"] :?> int)    
+    p.Serial <- %% "serial"
+    p.ProductType <- %% "product_type_name" |> Option.defaultValue ""
+    p.If_20 <- %% "i_f_minus20"
+    p.Ifon <- %% "i_f_plus20"
+    p.If50 <- %% "i_f_plus50"
+    p.Is_20 <- %% "i_s_minus20"
+    p.Isns <- %% "i_s_plus20"
+    p.Is50 <- %% "i_s_plus50"
+    p.I13 <- %% "i13"
+    p.I24 <- %% "i24"
+    p.I35 <- %% "i35"
+    p.I26 <- %% "i26"
+    p.I17 <- %% "i17"
+    p.In <- %% "not_measured"
+    p.IsReportIncluded <-  r.["production"] :?> bool
+    p.Flash <- r.["flash"] :?> byte[]
+    let n2 = r.["place"] :?> int
+    if p.N <> n2 then failwithf "N=%d != place=%d" p.N n2
+    p
+
+
+
+let getExportPartyProducts x party_id = 
+    let products = Array.init 96 (fun n -> old.Product.createNew(n) ) 
+    let cmd = new SQLiteCommand(x.conn)
+    cmd.CommandText <- "SELECT * FROM product WHERE party_id = @party_id;"
+    cmd.AddValue "party_id" dbt.Int64 party_id
+    let r = cmd.ExecuteReader()
+    while r.Read() do            
+        let p = readExportProduct r
+        if p.N >= 0 && p.N < 96 then
+            products.[p.N] <- p
+        else 
+            failwithf "%A" p 
+        let product_id = r.["product_id"] :?> int64
+            
+        let cmd = new SQLiteCommand(x.conn)
+        cmd.CommandText <- "UPDATE product SET old_product_id = @old_product_id WHERE product_id = @product_id;"
+        cmd.AddValue "old_product_id" dbt.String (string(p.Id))
+        cmd.AddValue "product_id" dbt.Int64 product_id
+        cmd.ExecuteNonQuery() |> ignore
+    r.Close()
+    products
+
+let readExportParty x (r:SQLiteDataReader)  =    
+    let (~%%) (s:string) : 'T option = 
+        if r.[s] = null then None else Some (r.[s] :?> 'T)
+    let p = old.Party.createNew ( r.["created_at"] :?> DateTime )
+    p.Name <- %% "note" |> Option.defaultValue ""
+    p.ProductType <- r.["product_type_name"] :?> string
+    p.PGS1 <- r.["concentration1"] :?> decimal
+    p.PGS2 <- r.["concentration2"] :?> decimal
+    p.PGS3 <- r.["concentration3"] :?> decimal
+    
+    let party_id = r.["party_id"] :?> int64
+    p.Products <- 
+        getExportPartyProducts x party_id
+        |> Array.toList
+    p.ProductsSerials <- old.Party.getProductsSerials p.Products
+
+    let cmd = new SQLiteCommand(x.conn)
+    cmd.CommandText <- "UPDATE product SET old_party_id = @old_party_id WHERE party_id = @party_id;"
+    cmd.AddValue "old_party_id" dbt.String (string(p.Id))
+    cmd.AddValue "party_id" dbt.Int64 party_id
+    cmd.ExecuteNonQuery() |> ignore
+    p
+
+
+let syncronize () = 
+
+    let appName = "elco"
+    
+    let dbPath = 
+        let mutable s = Environment.GetEnvironmentVariable("MYAPPDATA") 
+        if s = "" then
+            s <- Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        Path.Combine(s, "Аналитприбор", appName)
+
+    let fileName = Path.Combine(dbPath, appName + ".sqlite" )    
+
+    let fileExitst = File.Exists fileName
+
+    printfn "database: %s" fileName  
+    if not fileExitst then
+        printfn "create sqlite database file"
+        SQLiteConnection.CreateFile fileName
+        
+    let conn = new SQLiteConnection( sprintf "Data Source=%s; Version=3;" fileName)
+    try 
+        conn.Open() 
+    with e ->
+        failwith e.Message        
+    
+    printfn "%A: initialize sqlite database" DateTime.Now
+    let cmd = new SQLiteCommand(conn, CommandText = initdb.sql)
+    cmd.ExecuteNonQuery() |> ignore
+
+    printfn "%A: read imported parties" DateTime.Now
+    cmd.CommandText <- "SELECT old_party_id FROM party WHERE old_party_id NOT NULL;"
+
+    let r = cmd.ExecuteReader()    
+    let importedParties =
+        seq{
+            while r.Read() do         
+                yield r.["old_party_id"] :?> string            
+        } |> Set.ofSeq
+    r.Close()
+
+    printfn "imported parties: %d" importedParties.Count    
+
+    printfn "%A: read old parties" DateTime.Now
+
+    let oldParties = 
+        EccCO.v2.Repository.getInfoList env.CurrentDirectory
+        |> List.sortBy(fun x -> x.Date.Ticks * (-1L))
+
+    printfn "%A: old parties: %d" DateTime.Now oldParties.Length 
+
+    let x = {   
+        conn = conn
+        importedOldParties = importedParties
+        oldParties = oldParties
+    }
+
+    printfn "%A: start import" DateTime.Now
+    importParties x
+    
+    let cmd = new SQLiteCommand(conn)
+    cmd.CommandText <- "SELECT * FROM party WHERE old_party_id ISNULL;"
+    let r = cmd.ExecuteReader()    
+    while r.Read() do         
+        readExportParty x r        
+        |> EccCO.v2.Repository.save env.CurrentDirectory 
+    r.Close()
+
+    let cmd = new SQLiteCommand(conn)
+    cmd.CommandText <- "SELECT * FROM last_party;"
+
+
+    conn.Close()
+
+    Process.Start(dbPath) |> ignore
